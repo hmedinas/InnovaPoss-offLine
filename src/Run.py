606bf7fa8285256a23edd421302995baa8fe0e2a
Hugo Medina <alexei38@gmail.com>
2017-11-20 17:23:54 +0100

    nuevos cambios

--- src/Run.py
+++ src/Run.py
@@ -270,10 +270,10 @@ def Devolucion() -> bool:
 
 if __name__=='__main__':
 
-    print(f'{os.getcwd()}')
-    config_path = r"C:\Dimatica\offLine\Config\Configuracion.config"
-    oLog=LogProceso()
-    oLog.StartLogging(config_path)
+    #print(f'{os.getcwd()}')
+    #config_path = r"C:\Dimatica\offLine\Config\Configuracion.config"
+    #oLog=LogProceso()
+    #oLog.StartLogging(config_path)
 
 
     logging.info("Mensaje")
